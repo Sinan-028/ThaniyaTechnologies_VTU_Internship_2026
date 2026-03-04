27cbdce951391afa71c929a304fe84de8e507c7d
Mahammad Sinan <sinanmhd909@gmail.com>
2026-03-04 10:01:28 +0530

    Day4: Added multiply function

---
+++ Day_4/app.js
@@ -0,0 +1,17 @@
+console.log("Day4 Git Practice");
+
+function add(a,b){
+    return a + b;
+}
+
+function subtract(a,b){
+    return a - b;
+}
+
+function multiply(a,b){
+    return a * b;
+}
+
+console.log(add(5,3));
+console.log(subtract(10,4));
+console.log(multiply(4,3));
